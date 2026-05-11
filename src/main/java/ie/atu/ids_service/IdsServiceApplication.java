cc03388dc81abed347a07aa7a0105b96f6ff00ab
Sami Hammar <G00393324@atu.ie>
2026-05-11 03:56:37 +0100

    First Commit

---
+++ src/main/java/ie/atu/ids_service/IdsServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.ids_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdsServiceApplication.class, args);
+    }
+
+}
